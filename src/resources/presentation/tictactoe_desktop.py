7d57dbe47f400413ac948793af0f7a28a1250540
Jezreel Kintino de Moraes <jezreel.moraes@asaas.com.br>
2025-08-16 18:01:09 -0300

    refactor: Aplicando BoardPosition em TicTacToeDesktop

--- src/resources/presentation/tictactoe_desktop.py
+++ src/resources/presentation/tictactoe_desktop.py
@@ -4,12 +4,14 @@ from PyQt5.QtWidgets import (QApplication, QGridLayout, QHBoxLayout, QLabel,
                              QMainWindow, QMessageBox, QPushButton,
                              QVBoxLayout, QWidget)
 
+from resources.core.board_position import BoardPosition
+from resources.core.constants import BOARD_SIZE
 from resources.core.tictactoe_core import TicTacToeCore
 
 
 class TicTacToeDesktop(QMainWindow):
 
-    SYMBOLS = {
+    _SYMBOLS = {
         TicTacToeCore.EMPTY: ' ',
         TicTacToeCore.PLAYER_X: 'X',
         TicTacToeCore.PLAYER_O: 'O'
@@ -17,55 +19,61 @@ class TicTacToeDesktop(QMainWindow):
 
     def __init__(self, app: QApplication, core: TicTacToeCore):
         super().__init__()
-        self.core = core
-        self.app = app
+        self._core = core
+        self._app = app
 
         self.setWindowTitle("Jogo da Velha")
 
-        self.central_widget = QWidget()
-        self.setCentralWidget(self.central_widget)
+        self._central_widget = QWidget()
+        self.setCentralWidget(self._central_widget)
         self.layout = QVBoxLayout()
-        self.central_widget.setLayout(self.layout)
+        self._central_widget.setLayout(self.layout)
 
-        self.info_label = QLabel("Vez do jogador: X")
-        self.layout.addWidget(self.info_label)
+        self._info_label = QLabel()
+        self.layout.addWidget(self._info_label)
 
-        self.grid_layout = QGridLayout()
-        self.layout.addLayout(self.grid_layout)
+        self._grid_layout = QGridLayout()
+        self.layout.addLayout(self._grid_layout)
 
-        self.buttons = []
-        for row in range(3):
-            for column in range(3):
+        self._buttons = []
+        for row in range(BOARD_SIZE):
+            for column in range(BOARD_SIZE):
+
                 button = QPushButton("")
                 button.setMinimumSize(80, 80)
+
                 button.setSizePolicy(
                     button.sizePolicy().Expanding,
                     button.sizePolicy().Expanding
                 )
 
+                position = BoardPosition(row=row, column=column)
                 button.clicked.connect(
-                    lambda _, index=row * 3 + column: self.handle_move(index)
+                    lambda _, button=button, position=position: self._handle_move(button, position)
                 )
 
-                self.grid_layout.addWidget(button, row, column)
-                self.buttons.append(button)
+                self._grid_layout.addWidget(button, row, column)
+                self._buttons.append(button)
 
-        self.restart_btn = QPushButton("Reiniciar")
-        self.restart_btn.clicked.connect(self.start)
+        self._restart_btn = QPushButton("Reiniciar")
+        self._restart_btn.clicked.connect(self._start)
 
         footer = QHBoxLayout()
         footer.addStretch()
-        footer.addWidget(self.restart_btn)
+        footer.addWidget(self._restart_btn)
 
         self.layout.addLayout(footer)
-        self.update_board()
 
-    def handle_move(self, index):
-        row, col = divmod(index, 3)
-        if self.core.winner:
+    def run(self):
+        self._start()
+        self.show()
+        sys.exit(self._app.exec_())
+
+    def _handle_move(self, button: QPushButton, position: BoardPosition):
+        if self._core.winner:
             return
 
-        if not self.core.play_move(row, col):
+        if not self._core.play_move(position):
             QMessageBox.warning(
                 self,
                 "Movimento inválido",
@@ -74,33 +82,37 @@ class TicTacToeDesktop(QMainWindow):
 
             return
 
-        self.update_board()
-        if self.core.winner:
-            self.info_label.setText(
-                f"Jogador { self.SYMBOLS[self.core.winner] } venceu!"
+        self._update_button_label(button, position)
+
+        if self._core.winner:
+            self._info_label.setText(
+                f"Jogador { self._get_player_symbol(self._core.winner) } venceu!"
             )
 
             QMessageBox.information(
                 self,
                 "Fim de jogo",
-                f"Jogador { self.SYMBOLS[self.core.winner] } venceu!"
+                f"Jogador { self._get_player_symbol(self._core.winner) } venceu!"
             )
         else:
-            current = self.SYMBOLS[self.core.current_player]
-            self.info_label.setText(f"Vez do jogador: {current}")
+            current_player = self._SYMBOLS[self._core.current_player]
+            self._info_label.setText(f"Vez do jogador: {current_player}")
 
-    def update_board(self):
-        for idx, btn in enumerate(self.buttons):
-            row, col = divmod(idx, 3)
-            value = self.core.get_position_value(row, col)
-            btn.setText(self.SYMBOLS[value])
+    def _update_button_label(self, button: QPushButton, position: BoardPosition):
+        player = self._core.get_position_value(position)
+        player_symbol = self._get_player_symbol(player)
+        button.setText(player_symbol)
 
-    def start(self):
-        self.core.start()
-        self.info_label.setText("Vez do jogador: X")
-        self.update_board()
+    def _get_player_symbol(self, player) -> str:
+        return self._SYMBOLS[player]
 
-    def run(self):
-        self.start()
-        self.show()
-        sys.exit(self.app.exec_())
+    def _update_current_player_label(self):
+        player_symbol = self._get_player_symbol(self._core.current_player)
+        self._info_label.setText(f"Vez do jogador: { player_symbol }")
+
+    def _start(self):
+        self._core.start()
+        self._update_current_player_label()
+
+        for button in self._buttons:
+            button.setText("")
